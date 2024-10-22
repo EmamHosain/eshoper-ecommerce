56d887114dafbb45d0bc46091229cb2c5b8691fc
Emam hossain <hossainemam12345@gmail.com>
2024-10-22 12:20:19 +0600

    user google with login isshue fixed

--- app/Http/Controllers/User/GoogleAuthController.php
+++ app/Http/Controllers/User/GoogleAuthController.php
@@ -19,25 +19,41 @@ class GoogleAuthController extends Controller
     {
         try {
             $google_user = Socialite::driver('google')->user();
+
+            // Check if a user exists with the same google_id
             $user = User::where('google_id', $google_user->getId())->first();
 
             if (!$user) {
-                $new_user = User::create([
-                    'first_name' => $google_user->getName(),
-                    'email' => $google_user->getEmail(),
-                    'google_id' => $google_user->getId(),
-                ]);
+                // If no user with google_id, check if the email already exists
+                $user = User::where('email', $google_user->getEmail())->first();
 
-                Auth::login($new_user);
+                if (!$user) {
+                    // If no user with the same email, create a new user
+                    $new_user = User::create([
+                        'first_name' => $google_user->getName(),
+                        'email' => $google_user->getEmail(),
+                        'google_id' => $google_user->getId(),
+                    ]);
 
-                return redirect()->route('user_dashboard');
+                    Auth::login($new_user);
+                } else {
+                    // If a user with the same email exists but without google_id, update the google_id
+                    $user->update([
+                        'google_id' => $google_user->getId(),
+                    ]);
+                    Auth::login($user);
+                }
             } else {
+                // If user with google_id exists, log them in
                 Auth::login($user);
-                return redirect()->route('user_dashboard');
             }
+
+            return redirect()->route('user_dashboard');
         } catch (\Throwable $th) {
             dd($th->getMessage());
         }
-
     }
+
+
+
 }
